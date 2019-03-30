90db78e8efeafd39f9ce555807e7f5b261498246
Antoni Kepinski <xxczaki@pm.me>
2019-03-30 22:55:02 +0100

    Fix tests

--- test.js
+++ test.js
@@ -5,25 +5,36 @@ import execa from 'execa';
 
 test('Test --help output', async t => {
 	const ret = await execa.shell('node ./cli.js --help');
+
 	t.regex(ret.stdout, /Usage/);
 });
 
 test('--version', async t => {
 	const {stdout} = await execa.shell('node ./cli.js --version');
+
 	t.true(typeof stdout.length === 'number');
 });
 
 test('--list', async t => {
-	const error = await t.throws(execa.shell('node ./cli.js --list'));
+	const error = await t.throwsAsync(() => {
+		return execa.shell('node ./cli.js --list');
+	});
+
 	t.regex(error.message, /found/);
 });
 
 test('Invalid URL', async t => {
-	const error = await t.throws(execa.shell('node ./cli.js --url foo'));
+	const error = await t.throwsAsync(() => {
+		return execa.shell('node ./cli.js --url foo');
+	});
+
 	t.regex(error.message, /URL/);
 });
 
 test('Invalid token', async t => {
-	const error = await t.throws(execa.shell('node ./cli.js --url google.com'));
+	const error = await t.throwsAsync(() => {
+		return execa.shell('node ./cli.js --url google.com');
+	});
+
 	t.regex(error.message, /node-bitly/);
 });
